410cb7cf75ac956f1b284f02c474444559651281
MANOHARAN123 <110726776+MANOHARAN123@users.noreply.github.com>
2023-07-18 12:58:41 +0530

    Create diagnol.java

---
+++ diagnol.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MainDiagnolSum {
+	public static int diagonalSum(int arr[][],int n) {
+		int sum=0;
+		for(int j=0;j<n;j++) {
+			sum=sum+arr[j][j];
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[][]=new int[n][n];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		int value=diagonalSum(arr,n);
+		System.out.println(value);
+
+	}
+
+}
